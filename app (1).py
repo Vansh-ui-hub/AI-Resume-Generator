ae68ef60c3a18c87995343e41b0ec570acbea005
Vansh-ui-hub <v0060960@gmail.com>
2026-07-28 12:45:45 +0530

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -146,8 +146,6 @@ if st.button('generate resume'):
             b64_image = base64.b64encode(img_file.read()).decode()
         data_uri = f"data:image/jpeg;base64,{b64_image}"
         code = code.replace("PROFILE_IMAGE_PLACEHOLDER", data_uri)
-
-    st.html(code , width="stretch" , unsafe_allow_javascript=True)
     
     response = agent.invoke({'messages': [{'role': 'user',"content": query}]})
     code = response['messages'] [-1].content [-1] ['text']
